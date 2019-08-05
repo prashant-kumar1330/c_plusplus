eaa5d58ef246b40e7c6f4f3be25c746c63b4b35d
prashant kumar <kumarprashant1330@gmail.com>
2019-08-05 21:32:28 +0530

    sorting algorithm

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+void insertion_sort(int arr[10],int n){
+     int steps=0;
+    for(int i=0;i<n-1;i++){
+        int curr = arr[i+1];
+        int j=i;
+        while(arr[j]>curr && j>=0){
+            steps++;
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = curr;
+    }
+    cout<<"steps "<<steps<<endl;
+     
+}
+int main(){
+     int x;
+   cin>>x;
+   int arr[x];
+   for(int i=0;i<x;i++){
+       int temp;
+       cin>>temp;
+       arr[i]=temp;
+   }   
+clock_t start =clock();
+    insertion_sort(arr,x);
+    clock_t end=clock()-start;
+  
+      cout<<"time required "<<end;
+         cout<<endl;
+    for(int i=0;i<x;i++){
+        cout<<arr[i]<<" ";
+    }
+   
+}
---
+++ sorting_algorithms/selection_sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int main(){
+   int x;
+   cin>>x;
+   int arr[x];
+   int steps=0;
+   for(int i=0;i<x;i++){
+       int temp;
+       cin>>temp;
+       arr[i]=temp;
+   }
+ clock_t start=clock();
+
+    for(int j=0;j<x;j++){
+      int min=1000000;
+      int min_index;
+      for(int i=j;i<x;i++){
+           
+         if(arr[i]<min){
+             steps++;
+         min = arr[i];
+         min_index = i;
+         }
+      }
+          int temp = arr[j];
+           arr[j] = min;
+          arr[min_index] = temp;
+    }
+ clock_t end=clock();
+    for(int j=0;j<x;j++){
+       cout<<arr[j]<<" ";
+    }
+    cout<<endl;
+    cout<<"time rquired "<<end-start<<endl;
+    cout<<"steps "<<steps;
+
+}
