d847d09b229c0f1c429586b96c7ed6fa1e863196
prashant kumar <kumarprashant1330@gmail.com>
2019-09-08 21:07:35 +0530

    graph using list

---
+++ graphs/graph_using_list.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class graph{
+ int V;
+ list<int> *l;
+ public:
+  graph(int v){
+  V=v;
+  l= new list<int>[v];
+
+  }
+
+ void addEdge(int u,int v,bool biDirectional=true){
+    l[u].push_back(v);
+    if(biDirectional){
+        l[v].push_back(u);
+    }
+ }
+
+ void printList(){
+     for(int i=0;i<V;i++){
+       cout<<i<<"-> ";
+       for(list<int>:: iterator it=l[i].begin();it!=l[i].end();it++){
+           cout<<*it<<",";
+       }
+        cout<<endl;
+     }
+ } 
+
+
+};
+int main(){
+graph g(7);
+g.addEdge(0,1,0);
+g.addEdge(0,2,0);
+g.addEdge(0,3,0);
+g.addEdge(1,4,0);
+g.addEdge(2,3,0);
+g.addEdge(2,4,0);
+g.addEdge(3,4,0);
+g.addEdge(5,6,0);
+g.printList();
+}
--- hashing/unordered_map.cpp
+++ hashing/unordered_map.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
-#include<cstring>
-#include<unordered_map>
+#include<bits/stdc++.h>
 using namespace std;
+class fruit{
+public:
+string name;
+int price;
+string city;
+
+fruit(string n,int p,string c){
+name=n;
+price=p;
+city=c;
+}
+
+
+};
 
 int main(){
 
@@ -9,7 +21,7 @@ unordered_map <string,int> Mymap;
 Mymap["mango"]=120;
 Mymap["apple"]=110;
 Mymap["banana"]=130;
-Mymap["chiku"]=140;
+Mymap["apple"]=140;
 for(auto i=Mymap.begin();i!=Mymap.end();i++){
     cout<<i->first<<" "<<i->second<<endl;
 }
@@ -17,9 +29,17 @@ Mymap.at("mango") += 100; //this at function return refrence of the value
 for(auto i=Mymap.begin();i!=Mymap.end();i++){
     cout<<i->first<<" "<<i->second<<endl;
 }
-cout<<Mymap.bucket("banana");
-
 }
+/*
+cout<<Mymap.bucket("banana");
+unordered_map <string,fruit> fruits;
+fruit f("papaya",170,"delhi");
+fruits[f.name]=f;
+fruit fs =fruits["papaya"];
+cout<<fs.name<<" ";
+cout<<fs.price<<" ";
+ cout<<fs.city<<" ";
+ }*/
 /*
 unordered_map is an associated container that stores elements formed by combination of
  key value and a mapped value. The key value is used to uniquely identify the element
